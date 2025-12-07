63433f6bbc0501c6f66e6e72d8b36f8cb0f77378
Ricardo Bermudez Tellez <39892056+ricardober93@users.noreply.github.com>
2025-12-07 18:33:04 -0500

    refactor(auth/profile): implement dependency injection and service interfaces (#4)
    - Add DI container middleware and service interfaces
    - Refactor controllers to use injected services

---
+++ src/model/auth/auth-services.interface.ts
@@ -0,0 +1,7 @@
+import { AuthResponse } from "@/types";
+import { UserWithoutPassword } from "../user.model";
+
+export interface IAuthServices {
+    login: (email: string, password: string) => Promise<AuthResponse>;
+    register: (input: { email: string; password: string; }) => Promise<UserWithoutPassword>;
+}
